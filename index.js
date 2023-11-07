fb97a8637179ad2afa63cc05502cbcc9487d33ba
aslamaz <muhammedazeez473@gmail.com>
2023-11-07 15:22:07 +0530

    insert-Delete-where mongodb

--- index.js
+++ index.js
@@ -40,22 +40,10 @@ const districtSchema = new Schema({
     required: false,
   },
 });
+
 const District = model("district", districtSchema);
 
-app.post("/District", async (req, res) => {
-  const { name, phone, username } = req.body;
-
-  district = new District({
-    name,
-    phone,
-    username,
-  });
-
-  await district.save();
-
-  res.json({ message: "District and phone inserted successfully" });
-});
-
+//model employee
 const employeeDetails = new Schema({
   employeeName: {
     type: String,
@@ -77,16 +65,77 @@ const employeeDetails = new Schema({
     type: String,
     required: true,
   },
-  designation:{
-    type:String,
-    required:true,
+  designation: {
+    type: String,
+    required: true,
   },
 });
 
 const employee = model("employeeDetails", employeeDetails);
 
+//model place
+const PlaceSchema = new Schema({
+  place: {
+    type: String,
+    required: true,
+  },
+  district_Id: {
+    type: String,
+    required: true,
+  }
+});
+
+const Places = model("PlaceSchema",PlaceSchema)
+
+//model student
+const studentinfo = new Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  department:{
+    type:String,
+    required:true
+  },
+  place:{
+    type:String,
+    required:true
+  },
+  district:{
+    type:String,
+    required:true
+  },
+  phone:{
+    type:Number,
+    required:true
+  },
+  studentId:{
+    type:String,
+    required:true
+  }
+})
+
+const studentDetails = model("studentinfo", studentinfo)
+
+app.post("/District", async (req, res) => {
+  const { name, phone, username } = req.body;
+
+  district = new District({
+    name,
+    phone,
+    username,
+  });
+
+  await district.save();
+
+  res.json({ message: "District and phone inserted successfully" });
+});
+
+
+
 app.post("/employee", async (req, res) => {
-  const { employeeName, address, phone, place, district,designation } = req.body;
+  const { employeeName, address, phone, place, district, designation } =
+    req.body;
 
   employeevalues = new employee({
     employeeName,
@@ -97,7 +146,135 @@ app.post("/employee", async (req, res) => {
     designation,
   });
 
-  await employeevalues.save()
+  await employeevalues.save();
 
   res.json({ message: "employee details successfully inserted" });
 });
+
+app.post("/Place", async (req,res)=>{
+  const{place,district_Id}=req.body
+
+  placeDeatils = new Places({
+    place,
+    district_Id
+  })
+
+  await placeDeatils.save()
+
+  res.json({ message: "place successfully inserted" });
+})
+
+app.post("/studentinfo",async(req,res)=>{
+  const{name,department,place,district,phone,studentId}=req.body
+
+  students = new studentDetails({
+    name,
+    department,
+    place,
+    district,
+    phone,
+    studentId,
+  })
+    await students.save()
+
+    res.json({message:"student details successfuly inserted "})
+
+})
+
+
+
+//get employee
+app.get("/employee",async(req,res)=>{
+  let details = await employee.find()
+ res.send(details).status(200)
+})
+
+//get district
+app.get("/District",async(req,res)=>{
+  let district = await District.find()
+  res.send(district).status(200)
+})
+
+app.get("/District/getOne",async(req,res)=>{
+  
+  let district = await District.findOne({ username : 'abcd'})
+  res.send(district).status(200)
+})
+
+//get student
+app.get("/Getstudentinfo",async(req,res)=>{
+  let students = await studentDetails.find()
+  res.send(students).status(200)
+})
+
+app.get("/GetOneStudentinfo",async(req,res)=>{
+  let students = await studentDetails.findOne({department:"automobile"})
+  res.send(students).status(200)
+})
+
+
+
+//delete place
+app.delete('/Place/:id', async (req, res) => {
+  try {
+    const placeId = req.params.id;
+    console.log(placeId);
+
+    
+    const deletedPlace = await Places.findByIdAndDelete(placeId);
+
+    if (!deletedPlace) {
+      return res.status(404).json({ message: 'State not found' });
+    }
+
+    res.json({ message: 'Place deleted successfully', deletedPlace });
+  } catch (err) {
+    console.error('Error deleting district:', err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+})
+
+//delete student
+app.delete("/studentinfo/:",async(req,res)=>{
+  try{
+    const studentName = req.params.name
+    const deleteStudent = await studentDetails.findByNameAndDelete(studentName)
+    if(!deleteStudent){
+      return res.status(404).json({message:"student not found"})
+    }
+    res.json({message:"student deleted succesfully",deleteStudent})
+  }catch(err){
+    console.error("Error deleting district:",err);
+    res.status(500).json({message:"internal server error"})
+  }
+})
+
+//get places using where condition
+app.get('/Place/:id', async (req, res) => {
+  try {
+    const districtId = req.params.id
+    const place = await Places.find({ district_Id: districtId })
+    if (place.length === 0) {
+      return res.status(404).json({ message: 'Place not found' })
+    }
+    res.send(place).status(200)
+  } catch (err) {
+    console.error('Error deleting state:', err)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+})
+
+//get student using where condition
+app.get('/Studentinfo/:name',async(req, res)=>{
+  try{
+    const studentName = req.params.name
+    const student = await studentDetails.find({name:studentName})
+    if(student.length === 0){
+      return res.status(404).json({ message:"student not found"})
+    }
+    res.send(student).status(200)
+  }catch(err){
+    console.error('Error deleting state:',err)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+})
